648037a5caea8b9d1636f7ea5c3a950f43633321
toledolautaro <lautarotoledo.dev@gmail.com>
2021-08-28 11:30:14 -0300

    agrega nuevas secciones

--- js/main.js
+++ js/main.js
@@ -22,3 +22,46 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/**CHANGE BACKGROUND HEADER**/
+function scroolHeader()
+{
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200)
+    {
+        nav.classList.add('scroll-header')
+    }
+    else
+    {
+        nav.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scroolHeader)
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
